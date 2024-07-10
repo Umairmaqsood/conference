560a5142c86d6ec6d55d7323a61d065ce3dc9b92
Umairmaqsood <umairmaqsood089@gmail.com>
2024-07-10 15:08:44 +0500

    change huge data

--- cheif-organizer.php
+++ cheif-organizer.php
@@ -1,53 +1,77 @@
 <?php
+
 include("conn.php");
-include("header.php");
+
+include ("header.php");
+
 ?>
 
 <main class="main">
-    <!-- Page Title -->
-    <div
+      <!-- Page Title -->
+      <div
         class="page-title"
         data-aos="fade"
-        style="background-image: url(assets/img/page-title-bg.webp); text-align: center;"
-    >
+        style="background-image: url(assets/img/page-title-bg.webp)"
+      >
         <div class="container position-relative">
-            <h1 style="color:white">Chief Organizer</h1>
-            <nav class="breadcrumbs">
-                <ol>
-                    <li><a href="index.php">Home</a></li>
-                    <li class="current">Chief Organizer</li>
-                </ol>
-            </nav>
+          <h1 style="color:white">Chief Organizer</h1>
+          
+          <nav class="breadcrumbs">
+            <ol>
+              <li><a href="index.php">Home</a></li>
+              <li class="current">Chief Organizer</li>
+            </ol>
+          </nav>
         </div>
-    </div>
-    <!-- End Page Title -->
+      </div>
+      <!-- End Page Title -->
 
-    <!-- Events Section -->
-    <section id="events" class="events section">
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
+      <!-- Events Section -->
+      <section id="events" class="events section">
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
             <div class="row gy-4">
-                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align: center;">
-                    <div class="member" style="text-align: center;">
-                        <img src="assets/img/speakers/tehmina.jpeg" class="img-fluid rounded-circle" alt="" style="display: block; margin: 0 auto;" />
-                        <div class="member-info" style="text-align: center;">
-                            <div class="member-info-content" style="text-align: center;">
-                                <h4>Dr. Tehmina Karamat Khan</h4>
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/tehmina.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                   Dr. Tehmina Karamat Khan
+                                </h4>
+                                <h5>
+                                   Assistant Professor, FUSST
+                                </h5>
                             </div>
-                            <div class="social" style="text-align: center;">
-                                <a href="" style="margin: 0 10px;"><i class="bi bi-twitter"></i></a>
-                                <a href="" style="margin: 0 10px;"><i class="bi bi-facebook"></i></a>
-                                <a href="" style="margin: 0 10px;"><i class="bi bi-instagram"></i></a>
-                                <a href="" style="margin: 0 10px;"><i class="bi bi-linkedin"></i></a>
-                            </div>
+                           
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-    <!-- /Events Section -->
-</main>
 
+
+
+                <!-- <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/tehmina.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                   Dr. Tehmina Karamat Khan
+                                </h4>
+                                <h5>
+                                   Assistant Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div> -->
+              
+        </div>
+            </div>
+          
+      </section>
+      <!-- /Events Section -->
+    </main>
 <?php
 include("footer.php");
 ?>
--- organizing-committee.php
+++ organizing-committee.php
@@ -26,128 +26,271 @@ include ("header.php");
       </div>
       <!-- End Page Title -->
 
-      <!-- Events Section -->
       <section id="events" class="events section">
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-          <div class="row">
-            <div class="col-md-6">
-              <img
-                src="assets/img/speaker.jpg"
-                alt="Speaker"
-                class="img-fluid"
-              />
-            </div>
-
-            <div class="col-md-6">
-              <div class="details">
-                <h2>First International Conference on Computer Science and Engineering, Islamabad, Pakistan</h2>
-                <h6>September 29 – 30, 2024 at Foundation University Islamabad, School of Science and Technology (FUSST)
-</h6>
-                <div class="social">
-                  <!-- <a href=""><i class="bi bi-twitter"></i></a> -->
-                  <a href="https://www.facebook.com/fuislamabad/"><i class="bi bi-facebook"></i></a>
-                  <a href="https://www.instagram.com/foundationuniversityislamabad/?hl=en"><i class="bi bi-instagram"></i></a>
-                  <a href="https://www.linkedin.com/school/fuislamabad/mycompany/"><i class="bi bi-linkedin"></i></a>
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
+            <div class="row gy-4">
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/shaheen.png" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Prof Dr. Muhammad Shaheen
+                                </h4>
+                                <h5>
+                                   DEAN FEIT, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
                 </div>
-                <p  style="color:black;">
-                Foundation University Islamabad is organizing its first international conference
-                 with the name of “Fusst International Conference on Computer Science and Engineering 
-                 (FICSE)” from September 29 – 30, 2024 at Foundation University Islamabad, School of 
-                 Science and Technology. The conference will bring together Government, industry,
-                  Academia, and civil society to explore the role of state of the art technologies 
-                  in the broad field of Information Technology, electrical, instrumentation and 
-                  communication engineering. FICSE aims to bring together researchers, practitioners
-                   and experts from academia and industry to focus on recent global advancement.
-                </p>
 
+
+                
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/sajid.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Sajid Ali khan
+                                </h4>
+                                <h5>
+                               HOD SE, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
               
-              </div>
-            </div>
-          </div>
         </div>
+            </div>
+          
       </section>
-      <!-- /Events Section -->
-
-
-
 
       <section id="events" class="events section">
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="details">
-              <h5>The conference will mainly include the following sessions:</h5>
-                <p style="color:black;">&#8226; Research paper presentation sessions</p>
-                <p style="color:black;">&#8226; Technology presentation sessions</p>
-                <p style="color:black;">&#8226; Discussion sessions</p>
-                <p style="color:black;">&#8226; Paid Stalls/ Booths from the concerned stakeholders</p>
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
+            <div class="row gy-4">
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/rehman.jpg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Engr. Abdur Rehman
+                                </h4>
+                                <h5>
+                                HOD EE, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
 
-                <p style="color:black;">
-                Researchers, technologists and engineers from industry, academia and civil society will be invited to submit their proposals/ research papers in response to the “Call for papers” advertised through press/ social media/ website and letters. All the submissions will be made to FICSE portal on or before August 15th, 2024. The submitted items will be scrutinized by Editors for their suitability in terms of innovation, creativity, presentation and relevance. The applicants will be informed about the acceptance of their research item on or before 1st September 2024 after which the presenter will prepare a 20-30 minute presentation for on-site or virtual seminar. The decision of FISCE Editorial on the scrutiny of the research items will be final.
-                </p>
 
-                <p  style="color:black;">
-                The conference will also include 6 keynote sessions for which the international/national speakers will be invited. The international speakers will make virtual presentations whereas the national speakers will present in the live session.
-                </p>
-
-                <p  style="color:black;">
-                All the accepted and presented research papers/ workshops/ one-pager of keynote talks will be published in the conference proceedings. All papers will be peer-reviewed and, if accepted, extended version will published in the Foundation University Journal of Engineering and Applied Sciences (“Y-Category” HEC recognized journal).                </p>
-
-                <p  style="color:black;">
-                Every attendee of the conference will be awarded a certificate of participation, a conference kit and a FICSE bag in recognition of his participation in the conference. Best paper/ product award will be given to the presenters scored the highest by the judges of the conference. On every day of the conference, a guest of honor will be invited to distribute the certificates and awards. The MD Fauji Foundation will be the Chief Guest of the conference and will join the opening of the conference on 30th September 2024.                </p>
-              </div>
-            </div>
-            <!-- <div class="col-md-6">
-              <img
-                src="assets/img/speaker.jpg"
-                alt="Speaker"
-                class="img-fluid"
-              />
-            </div> -->
-          </div>
-          
+                
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/ateeq.png" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Ateeq Ur Rehman
+                                </h4>
+                                <h5>
+                                HOD ET, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+              
         </div>
-        
+            </div>
+          
       </section>
-      <!-- /Events Section -->
+
+      <section id="events" class="events section">
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
+            <div class="row gy-4">
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/shariq.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>                        
+Dr. Shariq Hussain  
+                                </h4>
+                                <h5>
+                                Associate Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
 
 
+                
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/arif.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Arif Jamal
+                                </h4>
+                                <h5>
+                                Associate Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+              
+        </div>
+            </div>
+          
+      </section>
+
+      <section id="events" class="events section">
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
+            <div class="row gy-4">
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/haneef.jpg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Muhammad Haneef  
+                                </h4>
+                                <h5>
+                                Associate Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
 
 
+                
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/aqeel.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Muhammad Aqeel Iqbal 
+                                </h4>
+                                <h5>
+                                Assistant Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+              
+        </div>
+            </div>
+          
+      </section>
+
+      <section id="events" class="events section">
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
+            <div class="row gy-4">
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/imran.png" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Imran Daud  
+                                </h4>
+                                <h5>
+                                Assistant Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
 
 
+                
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/maryam.jpg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Maryam Kausar  
+                                </h4>
+                                <h5>
+                                Assistant Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+              
+        </div>
+            </div>
+          
+      </section>
+
+      <section id="events" class="events section">
+        <div class="container" data-aos="fade-up" data-aos-delay="100">   
+            <div class="row gy-4">
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/najam.jpeg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Najmul Hassan
+                                </h4>
+                                <h5>
+                                Assistant Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
 
 
-
-    </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                
+                <div class="col-xl-6 col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100" style="text-align:center">
+                    <div class="member">
+                        <img src="assets/img/speakers/salma.jpg" class="img-fluid rounded-circle" alt="" />
+                        <div class="member-info">
+                            <div class="member-info-content">
+                                <h4>
+                                Dr. Salma Khan
+                                </h4>
+                                <h5>
+                                Assistant Professor, FUSST
+                                </h5>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+              
+        </div>
+            </div>
+          
+      </section>
    
+    </main>
 <?php
-
-
 include("footer.php");
-
-
 ?>
